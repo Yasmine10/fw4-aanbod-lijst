ceaefd920e0548eb8cf014b9e5b57dd10b2fd9a5
Yasmine De Wolf <yasmine.de.wolf@student.ehb.be>
2021-11-12 20:32:26 +0100

    setup project + added basic styling

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div>
+		<h1 class="h1">Aanbod te koop</h1>
+		<HouseCard />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import HouseCard from "@/components/HouseCard";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components: {HouseCard}
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+@use './assets/styles/main';
+
 </style>
