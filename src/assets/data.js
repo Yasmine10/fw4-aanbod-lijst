ff9e48bc8638ded99d314e531ae0b0d73627f8cd
Yasmine De Wolf <yasmine.de.wolf@student.ehb.be>
2021-11-13 13:29:56 +0100

    created array for the data

---
+++ src/assets/data.js
@@ -0,0 +1,74 @@
+const type = {
+    HOUSE: "Huis",
+    CHARMINGHOUSE: "Charmant huis",
+    APPARTMENT: "Appartement",
+    FARM: "Boerderij",
+}
+
+const data = [
+    {
+        id: 1,
+        img: "IMG-2.jpg",
+        location: "Putte",
+        type: type.HOUSE,
+        price: "435.000",
+        bedroomsNr: 3,
+        area: "651",
+    },
+    {
+        id: 2,
+        img: "IMG-1.jpg",
+        location: "Heist-op-den-Berg",
+        type: type.CHARMINGHOUSE,
+        price: "435.000",
+        bedroomsNr: 3,
+        area: "126",
+    },
+    {
+        id: 3,
+        img: "IMG.jpg",
+        location: "Schriek",
+        type: type.FARM,
+        price: "965.000",
+        bedroomsNr: 2,
+        area: "49.780",
+    },
+    {
+        id: 4,
+        img: "IMG-3.jpg",
+        location: "Putte",
+        type: type.APPARTMENT,
+        price: "348.000",
+        bedroomsNr: 2,
+        area: "89",
+    },
+    {
+        id: 5,
+        img: "IMG-2.jpg",
+        location: "Putte",
+        type: type.HOUSE,
+        price: "435.000",
+        bedroomsNr: 3,
+        area: "651",
+    },
+    {
+        id: 6,
+        img: "IMG-1.jpg",
+        location: "Heist-op-den-Berg",
+        type: type.CHARMINGHOUSE,
+        price: "435.000",
+        bedroomsNr: 3,
+        area: "126",
+    },
+    {
+        id: 7,
+        img: "IMG.jpg",
+        location: "Schriek",
+        type: type.FARM,
+        price: "965.000",
+        bedroomsNr: 2,
+        area: "49.780",
+    }
+]
+
+export default data;
