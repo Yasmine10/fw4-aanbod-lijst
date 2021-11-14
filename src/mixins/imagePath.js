798dd3e81b631277211c95c0d8deb7c909af9bd0
Yasmine De Wolf <yasmine.de.wolf@student.ehb.be>
2021-11-14 13:55:54 +0100

    created mixin to be able to display the images

--- src/mixins/imagePath.js
+++ src/mixins/imagePath.js
@@ -1,5 +1,6 @@
 export const imagePath = {
     methods: {
+        // function to be able to show an image in components easily
         getImagePath(item) {
             return require(`../assets/images/${item}`);
         }
